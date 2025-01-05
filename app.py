5574515eff28e000c4761cfb818f593943584a77
vikasjangid10 <v7776139@gmail.com>
2025-01-05 17:10:38 +0530

    second

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 )
 
 st.title("Phidata Video AI Summarizer Agent 🎥🎤🖬")
-st.header("Powered by Gemini , developed by vikas")
+st.header("Powered by Gemini , developed by vikas jangid")
 
 @st.cache_resource
 def initialize_agent():
